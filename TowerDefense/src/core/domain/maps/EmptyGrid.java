68a2ffbc8eaaa86b2d681e4445516eba0f545f27
faramarzi-mojtaba <faramarzi.mojtaba@gmail.com>
2014-10-08 12:28:04 -0400

    viki codes commit by mojtaba

---
+++ TowerDefense/src/core/domain/maps/EmptyGrid.java
@@ -0,0 +1,34 @@
+package core.domain.maps;
+
+import java.awt.Graphics;
+
+public class EmptyGrid extends Grid {
+
+	public EmptyGrid(int newheiht, int newwidth){
+			super();
+			height = newheiht;
+			width = newwidth;
+		}
+
+		
+		@Override
+	public	void draw(Graphics g) {
+			
+			for (int i = 0; i < width * sizeOfUnit; i += sizeOfUnit) {
+				for (int j = 0; j < width  * sizeOfUnit; j += sizeOfUnit) {
+					g.drawLine(i, 0, i, height * sizeOfUnit);
+					g.drawLine(0, j, width * sizeOfUnit, j);
+				}
+			}
+
+			g.drawLine(width * sizeOfUnit - 1, height * sizeOfUnit - 1,
+					width * sizeOfUnit - 1, 0); // x1y1 x2y2
+
+			g.drawLine(0, height * sizeOfUnit - 1, width * sizeOfUnit - 1,
+					height * sizeOfUnit - 1);
+
+		}
+
+	}
+
+	
