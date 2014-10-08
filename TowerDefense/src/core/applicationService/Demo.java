0280ca8b77e64ed1cf0abd350f07d4fc71f5ecce
faramarzi-mojtaba <faramarzi.mojtaba@gmail.com>
2014-10-08 16:40:18 -0400

    commit test

--- TowerDefense/src/core/applicationService/Demo.java
+++ TowerDefense/src/core/applicationService/Demo.java
@@ -4,12 +4,14 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class Demo {
 	public static void main(String[] args) {
-		   AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DIConfiguration.class);
+		    AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DIConfiguration.class);
 	        IAplication app = context.getBean(MyApplication.class);
 	         
 	        app.processMessage("Hi Mojtaba", "mojtaba@abc.com");
 	         
 	        //close the context
+	        
+	        ////
 	        context.close();
 	}
 
