2d147f1c5529931405460ca7a753e8b274030a69
faramarzi-mojtaba <faramarzi.mojtaba@gmail.com>
2014-10-26 11:00:00 -0400

    Validation before save was added

--- TowerDefense/src/core/applicationService/mapServices/MapManager.java
+++ TowerDefense/src/core/applicationService/mapServices/MapManager.java
@@ -1,6 +1,10 @@
 package core.applicationService.mapServices;
 
+import core.applicationService.mapServices.connectivity.imp.ConnectivityService;
+import core.applicationService.mapServices.connectivity.imp.StartEndChecker;
 import core.domain.maps.Grid;
+import core.domain.waves.Position;
+
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
@@ -32,8 +36,23 @@ public class MapManager {
 	public String SaveMapIntoFle(Grid grid, String FileName) {
 
 		String errorMessage = "";
-
+		//validation part 
 		try {
+			StartEndChecker startEndChecker = new StartEndChecker();
+			if(!startEndChecker.hasEnd(grid.getContent()))
+				throw new Exception("There isn't any exit point in the map!");
+				
+			if(!startEndChecker.hasStart(grid.getContent()))
+				throw new Exception("There isn't any enterance point in the map!");
+
+			MapUtility mapUtility = new MapUtility();
+			Position start = mapUtility.getEnter(grid.getContent());
+			Position end = mapUtility.getExit(grid.getContent());
+			
+			ConnectivityService connectivityService = new ConnectivityService();
+			if(!connectivityService.isTherePath(start, end, grid.getContent()))
+				throw new Exception("There isn't any path between start and end points in the map!");
+			
 			FileOutputStream fout = new FileOutputStream(FileName);
 			ObjectOutputStream oos = new ObjectOutputStream(fout);
 			oos.writeObject(grid);
