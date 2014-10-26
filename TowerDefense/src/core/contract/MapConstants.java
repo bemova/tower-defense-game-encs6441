801ff9ea76d2f05d8857edb1a30b22ae05cf964d
Ali <ali1.sangari@gmail.com>
2014-10-26 11:16:21 -0400

    MapConstants

---
+++ TowerDefense/src/core/contract/MapConstants.java
@@ -0,0 +1,14 @@
+package core.contract;
+
+import java.awt.Color;
+
+
+public class MapConstants {
+	public static final Color PATH_COLOR = Color.GRAY;
+	public static final Color SCENERY_COLOR = Color.GREEN;
+	public static final Color ENTRANCE_COLOR = Color.RED;
+	public static final Color EXIT_COLOR = Color.BLUE;
+	
+	public static final Color MODERN_TOWER_COLOR = Color.ORANGE;
+	
+}
