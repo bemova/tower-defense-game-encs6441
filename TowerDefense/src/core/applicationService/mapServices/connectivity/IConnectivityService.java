43028ab857290a1f89871af703810a51f382b4eb
faramarzi-mojtaba <faramarzi.mojtaba@gmail.com>
2014-10-27 07:46:10 -0400

    javadoc was added

--- TowerDefense/src/core/applicationService/mapServices/connectivity/IConnectivityService.java
+++ TowerDefense/src/core/applicationService/mapServices/connectivity/IConnectivityService.java
@@ -6,10 +6,12 @@ import core.domain.waves.Position;
 public interface IConnectivityService {
 
 	/**
-	 * The main method.
-	 *
-	 * @param args
-	 *            the arguments
+	 * this interface was implemented only for dependency injection that will be implemented in the future
+	 * this sigature use for the class that can validate if there is nay path in our grid or not
+	 * @param start
+	 * @param end
+	 * @param matrix
+	 * @return boolean
 	 */
 	public boolean isTherePath(Position start, Position end, GridCellContentType[][] matrix);
 
