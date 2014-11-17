b85023e6bfa2c28f1b9430487fd7c26813105cb9
masoud.b <masoud.b@gmail.com>
2014-11-17 13:42:09 -0500

    Edit JavaDoc

--- TowerDefense/src/core/domain/warriors/defenders/towers/behaviours/BulletShootingBehaviour.java
+++ TowerDefense/src/core/domain/warriors/defenders/towers/behaviours/BulletShootingBehaviour.java
@@ -1,5 +1,9 @@
 package core.domain.warriors.defenders.towers.behaviours;
-
+/**
+ * Bullet shooting interface
+ * @author Team 5
+ *
+ */
 public interface BulletShootingBehaviour {
 	public boolean bulletShoot();
 
--- TowerDefense/src/core/domain/warriors/defenders/towers/behaviours/LineShootingBehaviour.java
+++ TowerDefense/src/core/domain/warriors/defenders/towers/behaviours/LineShootingBehaviour.java
@@ -1,5 +1,9 @@
 package core.domain.warriors.defenders.towers.behaviours;
-
+/**
+ * Line shooting interface
+ * @author Team 5
+ *
+ */
 public interface LineShootingBehaviour {
 	public boolean lineShoot();
 
