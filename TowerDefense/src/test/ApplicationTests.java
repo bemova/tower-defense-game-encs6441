f0d7b068ca0aaa808a31acea9d64865c9b33572f
faramarzi-mojtaba <faramarzi.mojtaba@gmail.com>
2014-12-01 22:38:12 -0500

    ui test suite

--- TowerDefense/src/test/ApplicationTests.java
+++ TowerDefense/src/test/ApplicationTests.java
@@ -7,9 +7,10 @@ import org.junit.runners.Suite.SuiteClasses;
 import test.core.applicationservice.MapServiceSuiteTest;
 import test.core.applicationservice.locationservices.LocationSuiteTest;
 import test.core.domain.warriors.TowersSuiteTest;
+import test.ui.game.SpecialDamageSuiteTest;
 
 @RunWith(Suite.class)
-@SuiteClasses({TowersSuiteTest.class, MapServiceSuiteTest.class, LocationSuiteTest.class})
+@SuiteClasses({TowersSuiteTest.class, MapServiceSuiteTest.class, LocationSuiteTest.class, SpecialDamageSuiteTest.class})
 public class ApplicationTests {
 	
 }
